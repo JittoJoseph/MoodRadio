2033384b965d1f7af785a1e0cda69c49b705b54f
Jitto Joseph <78639807+JittoJoseph@users.noreply.github.com>
2024-11-17 01:20:12 +0530

    remove unused state import

--- src/components/AudioVisualizer.tsx
+++ src/components/AudioVisualizer.tsx
@@ -1,5 +1,5 @@
 // src/components/AudioVisualizer.tsx
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
 
